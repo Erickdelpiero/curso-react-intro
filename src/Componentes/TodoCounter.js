f4e99f6d06463b60cce06391026ad03d8274917a
Erick Del Piero <Erick.gozales.r@uni.pe>
2023-11-30 11:30:48 -0500

    Mensaje de felicitaciones al terminar todos los Todo's

--- src/Componentes/TodoCounter.js
+++ src/Componentes/TodoCounter.js
@@ -1,9 +1,23 @@
+import { FcApproval } from "react-icons/fc";
+
+
 function TodoCounter(props){
-    return(
-    <h1 className="todo-counter">
-        Completaste {props.completed} de los {props.total} Todo's totales
-    </h1>
-    );
+    if(props.total!==0){
+        return(
+            <h1 className="todo-counter">
+                Completaste {props.completed} de los {props.total} Todo's totales
+            </h1>
+            );
+    }
+    else{
+        return(
+            <h1 className="todo-counter">
+                <FcApproval />
+                Felicitaciones!!!<br />
+                Has culminado todos tus pendientes
+            </h1>
+            );
+    }
     }
 
 export {TodoCounter}
